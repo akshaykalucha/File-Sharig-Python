2431f97dfa5eabedc59e8f2549d46cc1cb77d045
akshaykalucha3 <akshaykalucha@gmail.com>
2022-03-30 22:49:55 +0530

    updates

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import utils
 import server, client
 
 def main():
+    mode = utils.choice_input("Would you like this machine to be the file sender or receiver?", ['s', 'r'])
     print("server")
 
 if __name__ == "__main__":
