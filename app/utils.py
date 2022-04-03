20f05cbe06f80e6bf2bc49ace283aa7795c213d9
akshaykalucha3 <akshaykalucha@gmail.com>
2022-04-03 22:24:20 +0530

    added utils

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,9 @@
 from typing import List
+
+def choice_input(query: str, choices: List[str]):
+    ret = input(f"{query} ({choices[0]}/{choices[1]}): ").lower()
+    if ret not in choices:
+        print("Invalid input. Please try again.")
+        ret = input(f"{query} ({choices[0]}/{choices[1]}").lower()
+
+    return ret
