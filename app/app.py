6bf3a74e77804a41e8f5876754ac5a178bacf7b2
akshaykalucha3 <akshaykalucha@gmail.com>
2022-04-04 22:25:56 +0530

    added updates

--- app/app.py
+++ app/app.py
@@ -4,3 +4,15 @@ import server, client
 
 def main() -> None:
     mode = utils.choice_input("Would you like this machine to be the file sender or receiver?", ['s', 'r'])
+    
+    if mode == 'r':
+        print("This machine will be receiving the files.")
+    ftserver = server.ReceivingServer(
+        input("Enter the path of the directory you would like to transfer your files TO: ")
+    )
+
+    ftserver.listen_and_connect_to_client()
+    ftserver.handshake()
+    ftserver.receive_dir()
+
+    del ftserver
