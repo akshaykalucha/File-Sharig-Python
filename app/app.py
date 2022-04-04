4254d61c7b5b9abac0d847aacfc49957b715fbe2
akshaykalucha3 <akshaykalucha@gmail.com>
2022-04-04 22:25:29 +0530

    added utils

--- app/app.py
+++ app/app.py
@@ -2,9 +2,5 @@ from time import sleep
 import utils
 import server, client
 
-def main():
+def main() -> None:
     mode = utils.choice_input("Would you like this machine to be the file sender or receiver?", ['s', 'r'])
-    print("server")
-
-if __name__ == "__main__":
-    main
