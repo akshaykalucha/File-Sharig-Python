7ae3f15e21901b2b316a52663f97faadd30cea0f
akshaykalucha3 <akshaykalucha@gmail.com>
2022-04-05 22:28:23 +0530

    added updates

--- app/server.py
+++ app/server.py
@@ -3,3 +3,14 @@ import os
 
 import protocol_consts
 import protocol_exceptions
+
+class ReceivingServer:
+    def __init__(self, dstdir: str) -> None:
+        self._DST = dstdir
+
+        self._HOST = socket.gethostbyname(socket.gethostname())
+        print("In order for the sender machine to locate this machine in the local network, the following local IP will be needed:", self._HOST)
+
+        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server_socket.bind((self._HOST, protocol_consts.PORT))
+        self._transfer_socket = None
