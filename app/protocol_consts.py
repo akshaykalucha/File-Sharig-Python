6634f1aff620f06eb2feaf908b6a28f182fd12a3
akshaykalucha3 <akshaykalucha@gmail.com>
2022-04-08 22:35:50 +0530

    added updates

--- app/protocol_consts.py
+++ app/protocol_consts.py
@@ -1,1 +1,7 @@
 PORT = 5000
+
+# Message constants ---
+BYTESIZE_MSG      = 2   # The size of a regular message.
+BYTESIZE_PATHLEN  = 16  # The size of the message holding file path data.
+BYTESIZE_NFILES   = 16  # The size of the message holding file quantity.
+BYTESIZE_FILESIZE = 128 # The size of the message holding the size of the file.
